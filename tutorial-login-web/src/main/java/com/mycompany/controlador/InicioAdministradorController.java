8a3d694588392b406556670c2c462c8a27fa43cc
Yonathan Bohorquez <ycbohorquez@ucundinamarca.edu.co>
2019-09-14 14:09:22 -0500

    Login faltando validaciones

---
+++ tutorial-login-web/src/main/java/com/mycompany/controlador/InicioAdministradorController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.controlador;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Yonathan
+ */
+@Named(value = "inicioAdministradorController")
+@Dependent
+public class InicioAdministradorController {
+
+    /**
+     * Creates a new instance of InicioAdministradorController
+     */
+    public InicioAdministradorController() {
+    }
+    
+}
---
+++ tutorial-login-web/src/main/java/com/mycompany/controlador/InicioDirectorController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.controlador;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Yonathan
+ */
+@Named(value = "inicioDirectorController")
+@Dependent
+public class InicioDirectorController {
+
+    /**
+     * Creates a new instance of InicioDirectorController
+     */
+    public InicioDirectorController() {
+    }
+    
+}
